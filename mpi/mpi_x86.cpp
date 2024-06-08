58d36219a7b6ec12a387a56b027bcdc4851d497f
xyh-myra <133238517+xyh-myra@users.noreply.github.com>
2024-06-08 16:38:09 +0800

    mpi

---
+++ mpi/mpi_x86.cpp
@@ -0,0 +1,447 @@
+#include<iostream>
+#include <stdio.h>
+#include<cstring>
+#include<typeinfo>
+#include <stdlib.h>
+#include<cmath>
+#include<mpi.h>
+#include<windows.h>
+using namespace std;
+#define N 1000
+#define NUM_THREADS 7
+float** A = NULL;
+
+long long head, tail, freq;
+
+void A_init() {    
+    A = new float* [N];
+    for (int i = 0; i < N; i++) {
+        A[i] = new float[N];
+    }
+    for (int i = 0; i < N; i++) {
+        A[i][i] = 1.0;
+        for (int j = i + 1; j < N; j++) {
+            A[i][j] = rand() % 1000;
+        }
+
+    }
+    for (int k = 0; k < N; k++) {
+        for (int i = k + 1; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                A[i][j] += A[k][j];
+                A[i][j] = (int)A[i][j] % 1000;
+            }
+        }
+    }
+}
+void A_initAsEmpty() {
+    A = new float* [N];
+    for (int i = 0; i < N; i++) {
+        A[i] = new float[N];
+        memset(A[i], 0, N * sizeof(float));
+    }
+
+}
+
+void deleteA() {
+    for (int i = 0; i < N; i++) {
+        delete[] A[i];
+    }
+    delete A;
+}
+
+void LU() {    //普通消元算法
+    for (int k = 0; k < N; k++) {
+        for (int j = k + 1; j < N; j++) {
+            A[k][j] = A[k][j] / A[k][k];
+        }
+        A[k][k] = 1.0;
+
+        for (int i = k + 1; i < N; i++) {
+            for (int j = k + 1; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+}
+
+
+double LU_mpi(int argc, char* argv[]) {  //块划分
+    double start_time = 0;
+    double end_time = 0;
+    MPI_Init(&argc, &argv);
+    int total = 0;
+    int rank = 0;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    MPI_Status status;
+    MPI_Comm_size(MPI_COMM_WORLD, &total);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int begin = N / total * rank;
+    int end = (rank == total - 1) ? N : N / total * (rank + 1);
+    if (rank == 0) {  //0号进程初始化矩阵
+        A_init();
+
+        for (j = 1; j < total; j++) {
+            int b = j * (N / total), e = (j == total - 1) ? N : (j + 1) * (N / total);
+            for (i = b; i < e; i++) {
+                MPI_Send(&A[i][0], N, MPI_FLOAT, j, 1, MPI_COMM_WORLD);//1是初始矩阵信息，向每个进程发送数据
+            }
+        }
+
+    }
+    else {
+        A_initAsEmpty();
+        for (i = begin; i < end; i++) {
+            MPI_Recv(&A[i][0], N, MPI_FLOAT, 0, 1, MPI_COMM_WORLD, &status);
+        }
+
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);  //此时每个进程都拿到了数据
+    start_time = MPI_Wtime();
+    for (k = 0; k < N; k++) {
+        if ((begin <= k && k < end)) {
+            for (j = k + 1; j < N; j++) {
+                A[k][j] = A[k][j] / A[k][k];
+            }
+            A[k][k] = 1.0;
+            for (j = 0; j < total; j++) { //
+                if (j != rank)
+                    MPI_Send(&A[k][0], N, MPI_FLOAT, j, 0, MPI_COMM_WORLD);//0号消息表示除法完毕
+            }
+        }
+        else {
+            int src;
+            if (k < N / total * total)//在可均分的任务量内
+                src = k / (N / total);
+            else
+                src = total - 1;
+            MPI_Recv(&A[k][0], N, MPI_FLOAT, src, 0, MPI_COMM_WORLD, &status);
+        }
+        for (i = max(begin, k + 1); i < end; i++) {
+            for (j = k + 1; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);	//各进程同步
+    if (rank == 0) {//0号进程中存有最终结果
+        end_time = MPI_Wtime();
+        printf("平凡MPI，块划分耗时：%.4lf ms\n", 1000 * (end_time - start_time));
+    }
+    MPI_Finalize();
+    return end_time - start_time;
+}
+
+double LU_mpi_async(int argc, char* argv[]) {  //非阻塞通信
+    double start_time = 0;
+    double end_time = 0;
+    MPI_Init(&argc, &argv);
+    int total = 0;
+    int rank = 0;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    MPI_Status status;
+    MPI_Comm_size(MPI_COMM_WORLD, &total);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int begin = N / total * rank;
+    int end = (rank == total - 1) ? N : N / total * (rank + 1);
+
+    if (rank == 0) { 
+        A_init();
+        MPI_Request* request = new MPI_Request[N - end];
+        for (j = 1; j < total; j++) {
+            int b = j * (N / total), e = (j == total - 1) ? N : (j + 1) * (N / total);
+
+            for (i = b; i < e; i++) {
+                MPI_Isend(&A[i][0], N, MPI_FLOAT, j, 1, MPI_COMM_WORLD, &request[i - end]);
+            }
+
+        }
+        MPI_Waitall(N - end, request, MPI_STATUS_IGNORE); 
+    }
+    else {
+        A_initAsEmpty();
+        MPI_Request* request = new MPI_Request[end - begin];
+        for (i = begin; i < end; i++) {
+            MPI_Irecv(&A[i][0], N, MPI_FLOAT, 0, 1, MPI_COMM_WORLD, &request[i - begin]); 
+        }
+        MPI_Waitall(end - begin, request, MPI_STATUS_IGNORE);
+
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    start_time = MPI_Wtime();
+    for (k = 0; k < N; k++) {
+        if ((begin <= k && k < end)) {
+            for (j = k + 1; j < N; j++) {
+                A[k][j] = A[k][j] / A[k][k];
+            }
+            A[k][k] = 1.0;
+            MPI_Request* request = new MPI_Request[total - 1 - rank]; //非阻塞传递
+            for (j = rank + 1; j < total; j++) { 
+
+                MPI_Isend(&A[k][0], N, MPI_FLOAT, j, 0, MPI_COMM_WORLD, &request[j - rank - 1]);
+            }
+            MPI_Waitall(total - 1 - rank, request, MPI_STATUS_IGNORE);
+            if (k == end - 1)
+                break; 
+        }
+        else {
+            int src = k / (N / total);
+            MPI_Request request;
+            MPI_Irecv(&A[k][0], N, MPI_FLOAT, src, 0, MPI_COMM_WORLD, &request);
+            MPI_Wait(&request, MPI_STATUS_IGNORE);        
+        }
+        for (i = max(begin, k + 1); i < end; i++) {
+            for (j = k + 1; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);	
+    if (rank == total - 1) {
+        end_time = MPI_Wtime();
+        printf("平凡MPI，块划分+非阻塞耗时：%.4lf ms\n", 1000 * (end_time - start_time));
+        //print(A);
+    }
+    MPI_Finalize();
+    return end_time - start_time;
+}
+
+double LU_mpi_plus(int argc, char* argv[]) {  //优化块划分
+    double start_time = 0;
+    double end_time = 0;
+    MPI_Init(&argc, &argv);
+    int total = 0;
+    int rank = 0;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    MPI_Status status;
+    MPI_Comm_size(MPI_COMM_WORLD, &total);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int begin = N / total * rank;
+    int end = (rank == total - 1) ? N : N / total * (rank + 1);
+    if (rank == 0) { 
+        A_init();
+
+        for (j = 1; j < total; j++) {
+            int b = j * (N / total), e = (j == total - 1) ? N : (j + 1) * (N / total);
+            for (i = b; i < e; i++) {
+                MPI_Send(&A[i][0], N, MPI_FLOAT, j, 1, MPI_COMM_WORLD);
+            }
+        }
+
+    }
+    else {
+        A_initAsEmpty();
+        for (i = begin; i < end; i++) {
+            MPI_Recv(&A[i][0], N, MPI_FLOAT, 0, 1, MPI_COMM_WORLD, &status);
+        }
+
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    start_time = MPI_Wtime();
+    for (k = 0; k < N; k++) {
+        if ((begin <= k && k < end)) {
+            for (j = k + 1; j < N; j++) {
+                A[k][j] = A[k][j] / A[k][k];
+            }
+            A[k][k] = 1.0;
+            for (j = rank + 1; j < total; j++) { 
+
+                MPI_Send(&A[k][0], N, MPI_FLOAT, j, 0, MPI_COMM_WORLD);
+            }
+            if (k == end - 1)
+                break;
+        }
+        else {
+            int src = k / (N / total);
+            MPI_Recv(&A[k][0], N, MPI_FLOAT, src, 0, MPI_COMM_WORLD, &status);
+        }
+        for (i = max(begin, k + 1); i < end; i++) {
+            for (j = k + 1; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);	
+    if (rank == total - 1) {
+        end_time = MPI_Wtime();
+        printf("平凡MPI，块划分优化耗时：%.4lf ms\n", 1000 * (end_time - start_time));
+    }
+    MPI_Finalize();
+    return end_time - start_time;
+}
+
+double LU_mpi_circle(int argc, char* argv[]) {  
+    double start_time = 0;
+    double end_time = 0;
+    MPI_Init(&argc, &argv);
+    int total = 0;
+    int rank = 0;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    MPI_Status status;
+    MPI_Comm_size(MPI_COMM_WORLD, &total);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {  
+        A_init();
+
+        for (j = 1; j < total; j++) {
+            for (i = j; i < N; i += total) {
+                MPI_Send(&A[i][0], N, MPI_FLOAT, j, 1, MPI_COMM_WORLD);
+            }
+        }
+
+    }
+    else {
+        A_initAsEmpty();
+        for (i = rank; i < N; i += total) {
+            MPI_Recv(&A[i][0], N, MPI_FLOAT, 0, 1, MPI_COMM_WORLD, &status);
+        }
+
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    start_time = MPI_Wtime();
+    for (k = 0; k < N; k++) {
+        if (k % total == rank) {
+            for (j = k + 1; j < N; j++) {
+                A[k][j] = A[k][j] / A[k][k];
+            }
+            A[k][k] = 1.0;
+            for (j = 0; j < total; j++) { 
+                if (j != rank)
+                    MPI_Send(&A[k][0], N, MPI_FLOAT, j, 0, MPI_COMM_WORLD);
+            }
+        }
+        else {
+            int src = k % total;
+
+            MPI_Recv(&A[k][0], N, MPI_FLOAT, src, 0, MPI_COMM_WORLD, &status);
+        }
+        int begin = k;
+        while (begin % total != rank)
+            begin++;
+        for (i = begin; i < N; i += total) {
+            for (j = k + 1; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);	
+    if (rank == 0) {
+        end_time = MPI_Wtime();
+        printf("平凡MPI,循环划分耗时：%.4lf ms\n", 1000 * (end_time - start_time));
+        //print(A);
+
+    }
+    MPI_Finalize();
+    return end_time - start_time;
+}
+double LU_mpi_pipeline(int argc, char* argv[]) { 
+    double start_time = 0;
+    double end_time = 0;
+    MPI_Init(&argc, &argv);
+    int total = 0;
+    int rank = 0;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    MPI_Status status;
+    MPI_Comm_size(MPI_COMM_WORLD, &total);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int begin = N / total * rank;
+    int end = (rank == total - 1) ? N : N / total * (rank + 1);
+    if (rank == 0) { 
+        A_init();
+
+        for (j = 1; j < total; j++) {
+            int b = j * (N / total), e = (j == total - 1) ? N : (j + 1) * (N / total);
+            for (i = b; i < e; i++) {
+                MPI_Send(&A[i][0], N, MPI_FLOAT, j, 1, MPI_COMM_WORLD);
+            }
+        }
+
+    }
+    else {
+        A_initAsEmpty();
+        for (i = begin; i < end; i++) {
+            MPI_Recv(&A[i][0], N, MPI_FLOAT, 0, 1, MPI_COMM_WORLD, &status);
+        }
+
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    start_time = MPI_Wtime();
+    for (k = 0; k < N; k++) {
+        if ((begin <= k && k < end)) {
+            for (j = k + 1; j < N; j++) {
+                A[k][j] = A[k][j] / A[k][k];
+            }
+            A[k][k] = 1.0;
+            MPI_Request* request = new MPI_Request[total - 1 - rank];  
+            for (j = rank + 1; j < total; j++) { 
+
+                MPI_Isend(&A[k][0], N, MPI_FLOAT, j, 0, MPI_COMM_WORLD, &request[j - rank - 1]);
+            }
+            MPI_Waitall(total - 1 - rank, request, MPI_STATUS_IGNORE);
+            if (k == end - 1)
+                break;
+        }
+        else {
+            int src = k / (N / total);
+            MPI_Request request;
+            MPI_Irecv(&A[k][0], N, MPI_FLOAT, src, 0, MPI_COMM_WORLD, &request);
+            MPI_Wait(&request, MPI_STATUS_IGNORE);        
+        }
+        for (i = max(begin, k + 1); i < end; i++) {
+            for (j = k + 1; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);	
+    if (rank == total - 1) {
+        end_time = MPI_Wtime();
+        printf("平凡MPI，流水线优化耗时：%.4lf ms\n", 1000 * (end_time - start_time));
+        //print(A);
+    }
+    MPI_Finalize();
+    return end_time - start_time;
+}
+void test(void(*func)()) {
+    A_init();
+    QueryPerformanceCounter((LARGE_INTEGER*)&head);
+    func();
+    QueryPerformanceCounter((LARGE_INTEGER*)&tail);
+
+}
+
+int main(int argc, char* argv[]) {
+
+    QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+
+    //test(LU);
+    //cout << (tail - head) * 1000 / freq << endl;
+    LU_mpi_circle(argc, argv);
+
+
+
+
+
+}
+
